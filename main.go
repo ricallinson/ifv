7893af2c7329b9aa9b496ff68fc6676e16c28079
Ric Allinson <ric@ricallinson.com>
2019-02-26 17:47:58 -0800

    Removed espeak

--- main.go
+++ main.go
@@ -9,16 +9,10 @@ import (
 func main() {
 	var storyYamlPath string
 	flag.StringVar(&storyYamlPath, "story", "", "The path to the location of the story file to use for the game.")
-	var textInterface bool
-	flag.BoolVar(&textInterface, "text", false, "Play the game via the console with text.")
 	flag.Parse()
 	if storyYamlPath == "" {
 		fmt.Println("You must supply a path to the location of the story file to use for the game.")
 		os.Exit(1)
 	}
-	if textInterface {
-		CreateGame(storyYamlPath, CreateRenderToConsole()).Play()
-	} else {
-		CreateGame(storyYamlPath, CreateRenderToEspeak()).Play()
-	}
+	CreateGame(storyYamlPath, CreateRenderToConsole()).Play()
 }
